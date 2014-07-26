e60b6a37cbeec85ae49cb364900c3e67fc9bcfb5
Ichus <Ichus.local>
2014-07-26 00:04:11 -0400

    Added controller and json show view to display a github user's repositories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  get "git_repos/:username" => "git_repos#show", as: "git_repo", defaults: { format: :json }
+
   root 'home#index'
 end
---
+++ test/controllers/git_repos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GitReposControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
