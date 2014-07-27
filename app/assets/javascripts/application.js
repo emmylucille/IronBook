a59a06ff99aa6cc018b47366cc489aa780c47a45
Emily Bond <emilymbond@yahoo.com>
2014-07-26 20:57:21 -0400

    building backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,14 +22,14 @@
 
 //MODEL:
 var Person = Backbone.Model.extend({
-  urlRoot: '/users'
+  urlRoot: '/api'
 });
 
 
 //COLLECTION:
 var Directory = Backbone.Collection.extend({
 	model: Person,
-	url: '/users',
+	url: '/api',
 	initialize: function() {
 
 	},
@@ -42,7 +42,7 @@ var PersonView = Backbone.View.extend({
 	template: _.template($('.profile-template').html() || ''),
 
   	initialize: function () {
- 		  this.listenTo(this.model, 'sync', this.render);
+
       this.model.fetch();
   	},
 
@@ -57,24 +57,14 @@ var PersonView = Backbone.View.extend({
 var DirectoryView = Backbone.View.extend({
 
 	initialize: function (){
-	  this.listenTo(this.collection, 'sync', this.render);
+
      	this.collection.fetch();
 	},
 
-	getUsers: function () {
-		var users = [];
-		_.each(this.collection.models, function(model) {
-			users.push(model.attributes);
-		});
-		return users;
-	},
 
 	render: function(){
 		var template = _.template($('.directory-template').html().trim());
 		var output = '';
-		_.each(this.getUsers(), function (user) {
-			output += template({user: user});
-		});
 		$('.directory-container').html(output);
 	}
 });
