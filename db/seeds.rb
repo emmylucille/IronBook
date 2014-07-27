14afe4f7d58070b48a35e93089f7a1642466e896
Emily Bond <emilymbond@yahoo.com>
2014-07-26 21:06:42 -0400

    building backbone framework

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(firstname: "John", lastname: "Do", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "JohnDo", twitter_url: "twitter.com/JohnDo",
+               linkedin_url: "linkedin.com/JohnDo", avatar_uid: "1",
+               cohort_class: "Front End", cohort_id: 1, user_id: 1)
+Student.create(firstname: "Bill", lastname: "Excellent", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "BillE", twitter_url: "twitter.com/BillE",
+               linkedin_url: "linkedin.com/BillE", avatar_uid: "2",
+               cohort_class: "Back End", cohort_id: 1, user_id: 2)
+Student.create(firstname: "Ted", lastname: "Adventure", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "TedE", twitter_url: "twitter.com/TedE",
+               linkedin_url: "linkedin.com/TedE", avatar_uid: "3",
+               cohort_class: "Back End", cohort_id: 1, user_id: 3)
+Student.create(firstname: "Joe", lastname: "Smith", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "JoeSmith", twitter_url: "twitter.com/JoeSmith",
+               linkedin_url: "linkedin.com/JoeSmith", avatar_uid: "4",
+               cohort_class: "Front End", cohort_id: 1, user_id: 4)
+Student.create(firstname: "MikeMike", lastname: "TheFro", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "TheFro", twitter_url: "twitter.com/TheFro",
+               linkedin_url: "linkedin.com/TheFro", avatar_uid: "5",
+               cohort_class: "Front End", cohort_id: 1, user_id: 5)
+Student.create(firstname: "Fat", lastname: "Zack", bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+               github_username: "FatZack", twitter_url: "twitter.com/FatZack",
+               linkedin_url: "linkedin.com/FatZack", avatar_uid: "6",
+               cohort_class: "Back End", cohort_id: 1, user_id: 6)                                                                    
